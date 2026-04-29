072d0f7ab81e9795a1489650a156f4d998cb59eb
Helene Schweitzer <schweitzer.hel@gmail.com>
2026-04-29 11:23:34 +0200

    Review and tidy stg_customers staging model

--- jaffle_shop_dbt/models/staging/stg_customers.sql
+++ jaffle_shop_dbt/models/staging/stg_customers.sql
@@ -2,10 +2,13 @@
 
 WITH source AS (
     SELECT * FROM {{ source('jaffle_shop', 'customers') }}
-)
+),
 
-SELECT
+customer AS (SELECT
     id AS customer_id,
     first_name,
     last_name
 FROM source
+)
+
+SELECT * FROM customer
